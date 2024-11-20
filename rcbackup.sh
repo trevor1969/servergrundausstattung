5a867c1ad6082ef178bad101d8e50f444288b16d
trevor1969 <51275045+trevor1969@users.noreply.github.com>
2024-11-20 17:08:51 +0100

    Update rcbackup.sh
    Korrekturen. Jetzt läuft's

--- rcbackup.sh
+++ rcbackup.sh
@@ -2,13 +2,13 @@ MMASCHINE="ubuntuserver2"
 AOrdner=$(date +"%Y-%m-%d-%H-%M")
 LLOGGE="/bin/bash /home/$USER/logger.sh"
 RREPO="DeinRCLONE-Repo"                       # Das bereits eingerichtete rclone-Repo
-RREPOSUB="BackupHomeLab/$MMASCHINE"           # Das Unterverzeichnis, in dem die Daten gespeichert werden sollen
-RREPOARCH="BackupHomeLab/Archiv/$MMASCHINE"   # Wo zu archivierenden Dateien abgelegt werden sollen
+RREPOSUB="BackupHomelab/$MMASCHINE"           # Das Unterverzeichnis, in dem die Daten gespeichert werden sollen
+RREPOARCH="BackupHomelab/Archiv/$MMASCHINE"   # Wo zu archivierenden Dateien abgelegt werden sollen
 
 $LLOGGE neu
 $LLOGGE "Starte Backup..."
 cp log.txt log.tmp
-rclone sync . $RREPOBackup:/$REPOSUB/ --log-file=/home/$USER/log.txt --exclude=log.txt --exclude=snap/ --backup-dir=$RREPO:/$RREPOARCH/$AOrdner -P
+rclone sync . "$RREPO:/$RREPOSUB/" --log-file=/home/$USER/log.txt --exclude=log.txt --exclude=snap/ --backup-dir="$RREPO:/$RREPOARCH/$AOrdner" -P
 $LLOGGE "Fertig. ERR: $?"
 $LLOGGE " "
 rm log.tmp

9c778da47bb3a22b7aabf95a8fb97e9a2d16b785
trevor1969 <51275045+trevor1969@users.noreply.github.com>
2024-11-24 16:11:45 +0100

    Update logcompress.sh
    Anpassung des Pfads der Log-Datei

--- logcompress.sh
+++ logcompress.sh
@@ -4,12 +4,12 @@ LLOGGE="$CCONS $LLOGG/logger.sh"         # Aufruf des Loggers
 
 $LLOGGE neu
 $LLOGGE "Starte Logcompress..."
-mv log.txt $(date +"%Y-%m-%d-%H-%M").log  # Bisheriges Log wird umbenannt
+mv $LLOGG/log.txt $LLOGG/$(date +"%Y-%m-%d-%H-%M").log  # Bisheriges Log wird umbenannt
 $LLOGGE "Umbenennung ERR: $?"
-7z a log.7z *.log
+7z a $LLOGG/log.7z $LLOGG/*.log
 $LLOGGE "Packen ERR: $?"
-rm *.log
-touch log.txt
+rm $LLOGG/*.log
+touch $LLOGG/log.txt
 $LLOGGE "Logcompress abgeschlossen."
 $LLOGGE "Den Verlauf findest Du in der letzten Datei in log.7z"
 $LLOGGE " "
